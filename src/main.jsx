671973c913edd72daeec677c484b2b43e1b6b8ea
tglspfdsfgs <tglspfdsfgs@gmail.com>
2023-07-05 21:05:48 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+import "./index.css";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction
+} from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      errorElement={<ErrorPage />}
+      loader={rootLoader}
+      action={rootAction}>
+        <Route 
+          index={true}
+          element={<Index/>}
+        />
+        <Route
+          path="contacts/:contactId"
+          loader={contactLoader}
+          action={contactAction}
+          element={<Contact />}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+          />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+          errorElement={<div>Oops! There was an error.</div>}
+        />
+    </Route>
+));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/root.jsx
@@ -0,0 +1,120 @@
+/* eslint-disable react-refresh/only-export-components */
+import {  Outlet, 
+          /*Link,*/ 
+          NavLink, 
+          Form, 
+          useLoaderData, 
+          useNavigation, 
+          redirect,
+          useSubmit,
+} from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { getContacts, createContact } from "../contacts";
+
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
+}
+
+export async function action() {
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
+
+export default function Root() {
+    const { contacts, q } = useLoaderData();
+    const navigation = useNavigation();
+    const inputRef = useRef();
+    const submit = useSubmit();
+
+    const searching =
+    navigation.location &&
+      new URLSearchParams(navigation.location.search).has("q");
+
+    useEffect(() => {
+      inputRef.current.value = q;
+    }, [q]);
+
+    return (
+      <>
+        <div id="sidebar">
+          <h1>React Router Contacts</h1>
+          <div>
+            <form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+                className={searching ? "loading" : ""}
+                //defaultValue={q}
+                ref={inputRef}
+                onChange={(event) => {
+                  const isFirstSearch = q == null;
+                  submit(event.currentTarget.form, {
+                    replace: !isFirstSearch,
+                  });
+                }}
+              />
+              <div
+                id="search-spinner"
+                aria-hidden
+                hidden={!searching}
+              />
+              <div
+                className="sr-only"
+                aria-live="polite"
+              ></div>
+            </form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                    <NavLink
+                      to={`contacts/${contact.id}`}
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                          ? "pending"
+                          : ""
+                      }
+                    >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
+        </div>
+        <div
+        id="detail"
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+      >
+          <Outlet />
+        </div>
+      </>
+    );
+  }
